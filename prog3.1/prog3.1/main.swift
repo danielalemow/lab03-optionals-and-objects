fadba1449731bcb8d7df51691bac53456fcdf426
Daniela Lemow <danielalemow@danielas-macbook-pro.local>
2018-07-21 18:00:48 +1200

    Added initial project files.

---
+++ prog3.1/prog3.1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  prog3.1
+//
+//  Created by Daniela Lemow on 21/07/18.
+//  Copyright © 2018 Daniela Lemow. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
