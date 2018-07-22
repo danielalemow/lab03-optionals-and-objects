694a23225f5461634d4c27847a8e247654f6bf5f
Daniela Lemow <danielalemow@dn-wlan-169-253.sns.otago.ac.nz>
2018-07-22 20:25:19 +1200

    Updated SimpleOptional so that operator methods and NIL definition were outside the class body.

--- prog3.1/prog3.1/SimpleOptional.swift
+++ prog3.1/prog3.1/SimpleOptional.swift
@@ -61,43 +61,45 @@ class SimpleOptional : CustomStringConvertible {
         self.hasValue = true
     }
     
-    /**
-     * Equality comparison operator between two SimpleOptionals
-     *
-     * Two optionals are considered the same if they both wrap
-     * a value or are both nil. The wrapped values are not compared.
-     * This is useful for checking whether a SimpleOptional has a
-     * value or not.
-     *
-     * - parameter left: The operand to the left of ==
-     * - parameter right: The operand to the right of ==
-     *
-     * - returns: Bool True if both optionals have a value or both
-     * have no value, returns false otherwise.
-     */
-    static func == (left: SimpleOptional, right: SimpleOptional) -> Bool {
-        if left.hasValue == right.hasValue {
-            return true
-        } else {
-            return false
-        }
-    }
-    
-    /*
-     * Non-equality comparison operator between two SimpleOptionals
-     *
-     * This operator inverts the logic of the == operator
-     *
-     * - parameter left: The operand to the left of !=
-     * - parameter right: The operand to the right of !=
-     *
-     * - returns: Bool True if one of the optionals has a value and
-     * the other doesn't, false otherwise.
-     */
-    static func != (left: SimpleOptional, right: SimpleOptional) -> Bool {
-        return !(left == right)
-    }
-    
-    // Define NIL as a SimpleOptional that has no value
-    let NIL: SimpleOptional = SimpleOptional()
 }
+
+/**
+* Equality comparison operator between two SimpleOptionals
+*
+* Two optionals are considered the same if they both wrap
+* a value or are both nil. The wrapped values are not compared.
+* This is useful for checking whether a SimpleOptional has a
+* value or not.
+*
+* - parameter left: The operand to the left of ==
+* - parameter right: The operand to the right of ==
+*
+* - returns: Bool True if both optionals have a value or both
+* have no value, returns false otherwise.
+*/
+func == (left: SimpleOptional, right: SimpleOptional) -> Bool {
+    if left.hasValue == right.hasValue {
+        return true
+    } else {
+        return false
+        
+    }
+}
+
+/*
+ * Non-equality comparison operator between two SimpleOptionals
+ *
+ * This operator inverts the logic of the == operator
+ *
+ * - parameter left: The operand to the left of !=
+ * - parameter right: The operand to the right of !=
+ *
+ * - returns: Bool True if one of the optionals has a value and
+ * the other doesn't, false otherwise.
+ */
+func != (left: SimpleOptional, right: SimpleOptional) -> Bool {
+    return !(left == right)
+}
+
+// Define NIL as a SimpleOptional that has no value
+let NIL: SimpleOptional = SimpleOptional()
