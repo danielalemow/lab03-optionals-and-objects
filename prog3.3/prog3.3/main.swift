56ba0d5c1619f5ad7a437862ee7b5162116cd926
Daniela Lemow <danielalemow@dn-wlan-169-253.sns.otago.ac.nz>
2018-07-23 12:23:35 +1200

    Initial main file for project.

---
+++ prog3.3/prog3.3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  prog3.3
+//
+//  Created by Daniela Lemow on 23/07/18.
+//  Copyright © 2018 Daniela Lemow. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
