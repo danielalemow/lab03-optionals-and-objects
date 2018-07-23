6b8a12da981d7cf36deaed269d49817479383658
Daniela Lemow <danielalemow@dn-wlan-169-253.sns.otago.ac.nz>
2018-07-23 12:14:53 +1200

    Updated main to mimic our SimpleOptional class using the actual optional operator.

--- prog3.2/prog3.2/main.swift
+++ prog3.2/prog3.2/main.swift
@@ -8,5 +8,52 @@
 
 import Foundation
 
-print("Hello, World!")
+//Declare variable x as an optional Int
+var x : Int?
+
+//Assign nil to x
+x = nil
+
+//Print x (The forcing avoids Xcode warning messages)
+print("x=\(x as Int?)")
+
+//Check if x has a value or not
+if x == nil {
+    print("x has no value")
+} else {
+    //if x has a value, unwrap that value and print it
+    print("x has a value of \(x!)")
+}
+
+//Assign a value of 3 to x
+x = 3
+
+//print x
+print("x=\(x as Int?)")
+
+//Check if x has a value or not
+if x != nil {
+    //If x has a value, unwrap that value
+    //and print it
+    print("x has a value of \(x!)")
+} else {
+    print("x has no value")
+}
+
+//Declare variable z as an optional Fraction
+var z: Fraction?
+
+//Assign a Fraction object 1/3 to z
+z = Fraction(num: 1, den: 3)
+
+//print z
+print("z=\(z as Fraction?)")
+
+//Check if z has a value or not
+if z != nil {
+    print("z has a value of \(z!)")
+    print("The decimal property of z's value is \(z!.decimal)")
+} else {
+    print("z has no value")
+}
 
